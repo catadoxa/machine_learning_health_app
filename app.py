a19c649c6496eaf5457433ff3b6dec6848de711f
Josh Kluthe <kluthej@oregonstate.edu>
2017-08-18 22:12:32 -0700

    and again

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from flask import jsonify
 import urllib
 import json
 from analysis.analyze import get_analysis
+import requests
 
 
 app = Flask(__name__)
@@ -26,19 +27,22 @@ def ml_data():
     print("hit service")
     req = request.get_json(force=True)
     req = dict(req)
-    print("dict")
+    #print("dict")
     #data = "analysis/insomnia_data.json"
     url = "http://web.engr.oregonstate.edu/~aluyorg/history.php"
-    url = "{}?username={}&password={}".format(url, req["user"], req["password"])
+    #url = "{}?username={}&password={}".format(url, req["user"], req["password"])
     #req_data = json.dumps(req).encode("utf8")
     #headers = {"Content-type": "application/json", "Accept": "text/plain"}
     #req = urllib.request.Request(url, req_data, headers)
     #print("create req")
     #resp = urllib.request.urlopen(req)
-    print("open req")
-    resp = urllib.request.urlopen(url)
-    print("req open?")
-    data = json.loads(resp.read())
+    #print("open req")
+    #resp = urllib.request.urlopen(url)
+    #print("req open?")
+    #data = json.loads(resp.read())
+    payload = {"username": req["user"], "password": req["password"]}
+    r = requests.get(url, params=payload)
+    data = r.json()
     data = get_analysis(data)
     #print(data)
     return data
