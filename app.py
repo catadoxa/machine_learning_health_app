a670508672db64769061933019afdbabf50833b4
Josh Kluthe <kluthej@oregonstate.edu>
2017-08-18 19:57:45 -0700

    add flask-cors

--- app.py
+++ app.py
@@ -8,19 +8,22 @@ from flask import jsonify
 import urllib
 import json
 from analysis.analyze import get_analysis
+from flask_cors import CORS, cross_origin
 
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@app.after_request
-def after_request(response):
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    return response
+#@app.after_request
+#def after_request(response):
+#    response.headers.add('Access-Control-Allow-Origin', '*')
+#    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+#    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+#    return response
 
-
+@cross_origin()
 @app.route("/api/ml_data", methods=["POST"])
 def ml_data():
     req = request.get_json(force=True)
