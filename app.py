6689308f0226475161ce918affcc9d1e414abaf7
Josh Kluthe <kluthej@oregonstate.edu>
2017-08-18 21:59:40 -0700

    test

--- app.py
+++ app.py
@@ -23,15 +23,19 @@ def after_request(response):
 
 @app.route("/api/ml_data", methods=["POST"])
 def ml_data():
+    print("hit service")
     req = request.get_json(force=True)
     req = dict(req)
+    print("dict")
     #data = "analysis/insomnia_data.json"
     url = "http://web.engr.oregonstate.edu/~aluyorg/history.php"
     url = "{}?username={}&password={}".format(url, req["user"], req["password"])
     req_data = json.dumps(req).encode("utf8")
     headers = {"Content-type": "application/json", "Accept": "text/plain"}
     req = urllib.request.Request(url, req_data, headers)
+    print("create req")
     resp = urllib.request.urlopen(req)
+    print("open req")
 #    resp = urllib.request.urlopen(url)
     data = json.loads(resp.read())
     data = get_analysis(data)
