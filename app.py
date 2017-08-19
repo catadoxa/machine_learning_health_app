8f0726aeef6144d10636d337d03b6b7b96354d61
Josh Kluthe <kluthej@oregonstate.edu>
2017-08-18 19:45:54 -0700

    fix headers

--- app.py
+++ app.py
@@ -28,11 +28,11 @@ def ml_data():
     #data = "analysis/insomnia_data.json"
     url = "http://web.engr.oregonstate.edu/~aluyorg/history.php"
     url = "{}?username={}&password={}".format(url, req["user"], req["password"])
-#    req_data = json.dumps(req).encode("utf8")
-#    headers = {"Content-type": "application/json", "Accept": "text/plain"}
-#    req = urllib.request.Request(url, req_data, headers)
-#    resp = urllib.request.urlopen(req)
-    resp = urllib.request.urlopen(url)
+    req_data = json.dumps(req).encode("utf8")
+    headers = {"Content-type": "application/json", "Accept": "text/plain"}
+    req = urllib.request.Request(url, req_data, headers)
+    resp = urllib.request.urlopen(req)
+#    resp = urllib.request.urlopen(url)
     data = json.loads(resp.read())
     data = get_analysis(data)
     print(data)
